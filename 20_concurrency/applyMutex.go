dc0e63537b79000240b15357b955aaa3e295a2cc
KenShiiii <62145672+KenShiiii@users.noreply.github.com>
2021-11-10 11:01:36 +0800

    Keep on learning

--- 20_concurrency/applyMutex.go
+++ 20_concurrency/applyMutex.go
@@ -9,21 +9,25 @@ import (
 
 var counter int
 var wg sync.WaitGroup
+var mu sync.Mutex
 
 
 func incrementer(){
-	wg.Add(1)
+	mu.Lock()
 	i := counter
 	runtime.Gosched()
 	i++
 	counter = i
 	fmt.Println("counter: ",counter,"NumOfGoRountine: ",runtime.NumGoroutine())
+	mu.Unlock()
 	wg.Done()
 }
 
 func main(){
 	counter = 0
-	for j := 0; j<100;j++{
+	const gs = 100
+	wg.Add(gs)
+	for j := 0; j<gs;j++{
 		go incrementer()
 
 	}
